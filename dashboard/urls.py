0c8a0b769a3517184244f735bc7abdc419862768
“BillMuriu” <njorogekamau63@gmail.com>
2022-11-03 07:32:45 +0000

    crispy_forms within the profile forms.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from .import views
 
 urlpatterns = [
     path('home', views.home, name='dashboard'),
+    path('profile', views.profile, name='profile'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,11 +3,12 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
 # Create your views here.
-
-
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
 
+##Local Imports
+from .forms import *
+from .models import *
 
 
 @login_required
@@ -16,3 +17,21 @@ def home(request):
     context = {}
 
     return render(request, 'dashboard/home.html', context)
+
+def profile(request):
+
+    context = {}
+
+    if request.method == 'GET':
+        form  = ProfileForm()
+        context['form'] = form
+        return render(request, 'dashboard/profile.html', context)
+
+    if request.method == 'POST':
+        form  =  ProfileForm(request.POST)
+
+        if form.is_valid():
+            pass
+
+
+    return render(request, 'dashboard/profile.html', context)
